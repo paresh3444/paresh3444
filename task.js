4666d6d32757c39c630985a0a3cc3ce57c40653b
pareshsolanki <pareshsolanki3335@yahoo.com>
2024-07-19 19:12:12 +0530

    task change

--- task.js
+++ task.js
@@ -1,30 +1,35 @@
 
 
-// Min-Max in Array
-let numbers = [10, 20, 30, 40, 50, 60];
+// // Min-Max in Array
+// let numbers = [10, 20, 30, 40, 50, 60];
 
-let min = Math.min(...numbers);
-let max = Math.max(...numbers);
+// let min = Math.min(...numbers);
+// let max = Math.max(...numbers);
 
-console.log("Minimum Number = ", min);
-console.log("Maximum Number = ", max);
+// console.log("Minimum Number = ", min);
+// console.log("Maximum Number = ", max);
 
 
-// Two Arrays Merge in Same Value
-let array1 = [1, 2, 3];
-let array2 = ['a', 'b', 'c'];
+// // Two Arrays Merge in Same Value
+// let array1 = [1, 2, 3];
+// let array2 = ['a', 'b', 'c'];
 
-let Arrays = array1.map((element, index) => element + array2[index]);
+// let Arrays = array1.map((element, index) => element + array2[index]);
 
-console.log(Arrays);
+// console.log(Arrays);
 
-// Give Two Arrays, Convert String and Create New Array
-const Array1 = [1, 2, 3];
-const Array2 = ['a', 'b', 'c'];
+// // Give Two Arrays, Convert String and Create New Array
+// const Array1 = [1, 2, 3];
+// const Array2 = ['a', 'b', 'c'];
 
-const Array = array1.map((element, index) => String(element) + String(array2[index]));
+// const Array = array1.map((element, index) => String(element) + String(array2[index]));
 
-console.log(Array);
+// console.log(Array);
+
+let data = Math.random() * 60 + 20;
+let a = data
+Math.round(a)
+console.log(a)
 
 
 
